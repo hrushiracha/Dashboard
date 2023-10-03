0d042c38201ae285234caa7511722b33251f1cd2
Hrushikesh Rachawar <146446601+hrushiracha@users.noreply.github.com>
2023-10-03 16:50:14 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import 
+{ BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill}
+ from 'react-icons/bs'
+ import 
+ { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line } 
+ from 'recharts';
+
+function Home() {
+
+    const data = [
+        {
+          name: '2016',
+          sale: 150,
+          profit: 50,
+        },
+        {
+          name: '2017',
+          sale: 180,
+          profit: 70,
+        },
+        {
+          name: '2018',
+          sale: 200,
+          profit: 100,
+        },
+        {
+          name: '2019',
+          sale: 230,
+          profit: 90,
+        },
+        {
+          name: '2020',
+          sale: 250,
+          profit: 110,
+        },
+        {
+          name: '2021',
+          sale: 280,
+          profit: 135,
+        },
+        {
+          name: '2022',
+          sale: 300,
+          profit: 180,
+        },
+      ];
+     
+
+  return (
+    <main className='main-container'>
+        <div className='main-title'>
+            <h3>DASHBOARD</h3>
+        </div>
+
+        <div className='main-cards'>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>PRODUCTS</h3>
+                    <BsFillArchiveFill className='card_icon'/>
+                </div>
+                <h1>2000</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>CATEGORIES</h3>
+                    <BsFillGrid3X3GapFill className='card_icon'/>
+                </div>
+                <h1>50</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>CUSTOMERS</h3>
+                    <BsPeopleFill className='card_icon'/>
+                </div>
+                <h1>900</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>ALERTS</h3>
+                    <BsFillBellFill className='card_icon'/>
+                </div>
+                <h1>8</h1>
+            </div>
+        </div>
+
+        <div className='charts'>
+            <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="sale" fill="#82ca9d" />
+                <Bar dataKey="profit" fill="#8884d8" />
+                </BarChart>
+            </ResponsiveContainer>
+
+            <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="sale" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="profit" stroke="#8884d8" activeDot={{ r: 8 }} />
+                </LineChart>
+            </ResponsiveContainer>
+
+        </div>
+    </main>
+  )
+}
+
+export default Home
